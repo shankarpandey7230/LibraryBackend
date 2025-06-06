64512841eb5cab4e411bfdda3aa2dfc96b60e632
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-06-07 02:29:18 +1000

    middlewares fixed

--- src/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-import { responseClient } from "./responseClient.js";
-
-export const errorHandler = (error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message;
-
-  responseClient({ req, res, statusCode, message });
-};
